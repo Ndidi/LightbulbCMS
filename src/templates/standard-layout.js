f59ea2e4ffcc8bd2bf2c55d95d93de4f373d63e1
Ndidi <ndidi.alaneme@gmail.com>
2018-03-26 13:18:02 +0800

    Basic Footer

--- src/templates/standard-layout.js
+++ src/templates/standard-layout.js
@@ -54,6 +54,67 @@ export default function TemplateWrapper ({
         </div>
       </div>
       <div>{children()}</div>
+      <footer id="footer" class="footer">
+        <div class="container">
+          <h2 class="h2">Stay up to date with Nugit</h2>
+          <div class="sm-spacing"></div>
+          <p class="p center-align whiter reduced">Don’t worry, we hate spam too!<br/>We promise we’ll only send you Nugit related content.</p>
+          <div class="sm-spacing"></div>
+          {/* <div class="w-embed w-script">
+            <script>
+              hbspt.forms.create({
+                portalId: '1859491',
+                formId: 'c107e007-d7b7-4988-a8c4-fcd7f94c0e6b',
+                css:'form{display: flex; align-items: baseline; -webkit-align-items: baseline; -ms-flex-align: baseline; justify-content: center; -webkit-justify-content: center; flex-wrap: wrap; -ms-flex-wrap: wrap; -webkit-flex-wrap: wrap;}'
+              });
+            </script>
+          </div> */}
+          <div class="lg-spacing"></div>
+        </div>
+        <div class="container grid"><a href="index.html" class="footer-logo w-hidden-tiny w-inline-block"><img src="images/logo-white.svg" width="80"/></a>
+          <div class="footer-col">
+            <div class="heading-small">Product</div>
+            <div class="sm-spacing"></div><a href="https://www.nugit.co/data-integrations" class="link-footer">Integrations</a>
+            <div class="sm-spacing"></div><a href="https://www.nugit.co/nugit-bug-reporting-policy" class="link-footer">Bug Bounty</a>
+            <div class="sm-spacing"></div><a href="request-demo.html" class="link-footer">Request Demo</a></div>
+          <div class="footer-col">
+            <div class="heading-small">Company</div>
+            <div class="sm-spacing"></div><a href="about.html" class="link-footer">About</a>
+            <div class="sm-spacing"></div><a href="https://nugit.co/about.html#hiring" class="link-footer">We&#x27;re Hiring</a>
+            <div class="sm-spacing"></div><a href="https://www.nugit.co/blog" class="link-footer">Blog</a></div>
+          <div class="footer-col">
+            <div class="heading-small">Resources</div>
+            <div class="sm-spacing"></div><a href="https://www.nugit.co/casestudies" class="link-footer">Case Studies</a>
+            <div class="sm-spacing"></div><a href="https://www.nugit.co/webinars" class="link-footer">Webinars</a>
+            <div class="sm-spacing"></div><a href="https://www.nugit.co/whitepapers" class="link-footer">White Papers</a>
+            <div class="sm-spacing"></div><a href="https://www.nugit.co/news" class="link-footer">Press</a>
+            <div class="sm-spacing"></div><a href="http://intercom.help/nugit" class="link-footer">Knowledge Base</a></div>
+          <div class="footer-col">
+            <div class="heading-small">Legal</div>
+            <div class="sm-spacing"></div><a href="https://app.nugit.co/privacy" class="link-footer">Privacy Policy</a>
+            <div class="sm-spacing"></div><a href="https://app.nugit.co/terms-of-service" class="link-footer">Terms of Service</a>
+            <div class="sm-spacing"></div><a href="https://app.nugit.co/acceptable-use-policy" class="link-footer">Acceptable Use Policy</a></div>
+          <div class="footer-col">
+            <div class="heading-small">Contact</div>
+            <div class="sm-spacing"></div><a href="mailto:info@nugit.co" class="link-footer">info@nugit.co</a>
+            <div class="sm-spacing"></div><a href="tel:+6562217754" class="link-footer">(+65) 6221 7754</a>
+            <div class="sm-spacing"></div><a href="https://www.google.com.sg/maps/place/55+Amoy+St,+Singapore+069881/@1.2799941,103.8445767,17z/data=!3m1!4b1!4m5!3m4!1s0x31da190d6385f2ad:0x763446d58e270bc7!8m2!3d1.2799887!4d103.8467654?hl=en" target="_blank" class="link-footer">55b Amoy St, S069881</a></div>
+        </div>
+        <div class="md-spacing"></div>
+        <div class="container grid valign">
+          <div class="copyright">
+            <div class="copyright-content">
+              <div class="footer-txt"><span class="made-in">© 2018 Nugit Ltd Pte.  </span></div>
+            </div>
+            <div class="made-in-content">
+              <div class="footer-txt"><span class="made-in">Made with </span></div>
+              <div class="heart-icon"></div>
+              <div class="footer-txt"><span class="made-in"> in Singapore</span></div>
+            </div>
+          </div>
+          <div><a href="https://twitter.com/nugit" target="_blank" class="tw-icon w-inline-block"></a><a href="https://www.facebook.com/nugit/" target="_blank" class="fb-icon w-inline-block"></a><a href="https://www.linkedin.com/company/3487776/" target="_blank" class="lk-icon w-inline-block"></a></div>
+        </div>
+      </footer>
     </div>
   )
 }
