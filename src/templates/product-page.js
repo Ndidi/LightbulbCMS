c988896a95ccfaab5d1703b77e6d946ca1621f3e
Ndidi <ndidi.alaneme@gmail.com>
2018-05-07 13:47:09 +0800

    Product scaffolding

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -1,29 +1,29 @@
 import React from "react";
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data; // data.markdownRemark holds our post data
-  const { frontmatter, html } = markdownRemark;
+export const ProductPageTemplate = ({
+  title,
+}) => {
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
+    <div>
+      
     </div>
   );
 }
 
-export const pageQuery = graphql`
-  query Product($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
+export default ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return(
+    <ProductPageTemplate
+      title={frontmatter.title}
+    />
+  )
+}
+
+export const productPageQuery = graphql`
+  query Product($id: String!) {
+    markdownRemark(id: { eq: $id }) {
       frontmatter {
-        path
         title
       }
     }
