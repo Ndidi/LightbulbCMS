b20a3635c84d251c4d04fda0bad4990bcc4048e0
Ndidi <ndidi.alaneme@gmail.com>
2018-05-08 13:10:50 +0800

    Data Storytelling page in place

--- src/templates/data-storytelling-page.js
+++ src/templates/data-storytelling-page.js
@@ -1,29 +1,132 @@
 import React from "react";
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data; // data.markdownRemark holds our post data
-  const { frontmatter, html } = markdownRemark;
-  return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
+export const DataStoryTellingPageTemplate = ({
+
+}) => {
+  return(
+    <div>
+      <header className="main-header story">
+        <div className="container grid story">
+          <div className="story-title">
+            <div className="hero-story-container">
+              <div className="hero nofixedwidth">Share beautiful stories that give meaning to your business’ data</div>
+              <div className="sm-spacing _30"></div><a href="request-demo.html" className="btn w-hidden-medium w-hidden-small w-hidden-tiny w-button">Request Demo</a></div>
+            <div className="good-story"></div>
+          </div>
+          <div className="story-description">
+            <div className="p">Humans are hardwired to share stories that provide real meaning. With information overload being the new evolutionary hurdle for businesses and individuals, only data storytelling can put a human perspective on the increasingly complex and rapidly changing world of the digital era.</div>
+            <div className="sm-spacing _30"></div>
+            <div className="quote">&quot;Data storytelling enables us to connect the dots, gain real insights.&quot;</div>
+            <div className="sm-spacing _30"></div>
+            <div className="p">Sharing data in spreadsheets and dashboards alone are not enough. They lack context and meaning for people. Data storytelling enables us to connect the dots, gain real insights and people can take ownership of the new knowledge. Nugit’s data stories are evidence-based reports that employ visuals and natural language that enable people to make sense of otherwise overwhelming amounts of complex information.</div>
+            <div className="sm-spacing _30"></div>
+            <div className="p">This realises the full potential of all data, providing infinite scaling and immediate insights. Everyone can focus on making the information work for them rather than the other way around.</div>
+            <div className="sm-spacing _30 w-hidden-main"></div><a href="#" className="btn w-hidden-main w-button">Request Demo</a></div>
+        </div>
+      </header>
+      <section className="white-section gray">
+        <div className="container">
+          <h2 className="h2">With a Data Storytelling Platform your data becomes</h2>
+          <div className="sm-spacing"></div>
+          <div className="sm-spacing"></div>
+          <div className="flex-container space-between">
+            <div className="shadow-box _3cols center-txt storybox">
+              <div className="illustration harmonised"></div>
+              <div className="sm-spacing"></div>
+              <h4 className="h4">Harmonised</h4>
+              <div className="sm-spacing"></div>
+              <p className="small-copy">Tap into one space for all your information to gain any perspective at any time. Scale information requests to the infinite.</p>
+            </div>
+            <div className="shadow-box _3cols center-txt storybox">
+              <div className="illustration evocative"></div>
+              <div className="sm-spacing"></div>
+              <h4 className="h4">Evocative</h4>
+              <div className="sm-spacing"></div>
+              <p className="small-copy">Connect the dots and jump directly to discussing insights, not masses of facts. Speak the language of your audience, making them comfortable to act.</p>
+            </div>
+            <div className="shadow-box _3cols center-txt storybox">
+              <div className="illustration valuable"></div>
+              <div className="sm-spacing"></div>
+              <h4 className="h4">Valuable</h4>
+              <div className="sm-spacing"></div>
+              <p className="small-copy">Accelerate productivity with automation of analysis and visualisation; and redeploy human resources to activities that create value.</p>
+            </div>
+          </div>
+          <div className="hg-spacing"></div>
+          <h2 className="h2">Common Questions</h2>
+          <div className="sm-spacing"></div>
+          <p className="p center-align">You can find the answers here</p>
+          <div className="md-spacing"></div>
+          <div data-w-id="5b89c3e3-2aff-bc3d-381f-8c1e38cd3460" className="divider"></div>
+          <div data-delay="300" data-w-id="5b89c3e3-2aff-bc3d-381f-8c1e38cd3461" className="faq w-dropdown">
+          <div className="faq-toggle w-dropdown-toggle">
+            <div className="h5">What is the difference between a dashboard and a data storytelling platform?</div>
+            <div data-w-id="5b89c3e3-2aff-bc3d-381f-8c1e38cd3465" className="arrow-down qa"></div>
+          </div>
+          <div data-w-id="5b89c3e3-2aff-bc3d-381f-8c1e38cd3466" className="faq-a-bg w-dropdown-list"><a href="#" className="faq-a w-dropdown-link">Dashboards create a burden on analytics teams by providing data alone. They do not tell you what it means, or how it can be used. They lack context and purpose, and they aren’t easy to share, particularly when information requests come from various internal and external stakeholders. For thousands of years humans have relied on storytelling to convey information, using narrative and visualisations to communicate. In contrast to dashboards, data storytelling combines all of this in a powerful tool that drives action, and gives a new level of ownership to knowledge.</a></div>
+        </div>
+          <div className="divider"></div>
+          <div data-delay="300" data-w-id="5b89c3e3-2aff-bc3d-381f-8c1e38cd346a"  className="faq w-dropdown">
+            <div className="faq-toggle w-dropdown-toggle">
+              <div className="h5">Is a Data Storytelling Platform the same as a business intelligence tool?</div>
+              <div  className="arrow-down qa"></div>
+            </div>
+            <div  className="faq-a-bg w-dropdown-list"><a href="#" className="faq-a w-dropdown-link">No, it goes beyond just collecting data which is primarily what BI tools achieve. A Data Storytelling Platform uses artificial intelligence to fill the gap between dashboards and stories that make people take ownership of the new knowledge. This realises the full potential of all data, providing infinite scaling and immediate insights. Everyone can focus on making the information work for them rather than the other way around.</a></div>
+          </div>
+          <div className="divider"></div>
+          <div data-delay="300" data-w-id="5b89c3e3-2aff-bc3d-381f-8c1e38cd3473"  className="faq w-dropdown">
+            <div className="faq-toggle w-dropdown-toggle">
+              <div className="h5">Do you have to be a data scientist or data analyst to create data stories?</div>
+              <div  className="arrow-down qa"></div>
+            </div>
+            <div  className="faq-a-bg w-dropdown-list"><a href="#" className="faq-a w-dropdown-link">No, with Nugit, anyone in your organisation can now become data storytellers. There is no need to learn specific coding skills to build a data story in our platform. Nugit relieves the burden placed on analytics teams so you can scale information requests across thousands of stakeholders, from customers to team members.</a></div>
+          </div>
+          <div className="divider"></div>
+          <div data-delay="300" data-w-id="5b89c3e3-2aff-bc3d-381f-8c1e38cd347e"  className="faq w-dropdown">
+            <div className="faq-toggle w-dropdown-toggle">
+              <div className="h5">Is Data Storytelling the same as Data Journalism?</div>
+              <div  className="arrow-down qa"></div>
+            </div>
+            <div  className="faq-a-bg w-dropdown-list"><a href="#" className="faq-a w-dropdown-link">No. Data Journalism refers to the use of numerical data, often visualised in charts and infographics, used to support and better explain a particular journalistic story or argument. Data Storytelling is an approach used to convey insights, where data, visualisations, and a narrative work in tandem to influence a specific audience.</a></div>
+          </div>
+          <div className="divider"></div>
+          <div data-delay="300" data-w-id="5b89c3e3-2aff-bc3d-381f-8c1e38cd3487"  className="faq w-dropdown">
+            <div className="faq-toggle w-dropdown-toggle">
+              <div className="h5">Why should I use data storytelling for my business?</div>
+              <div  className="arrow-down qa"></div>
+            </div>
+            <div  className="faq-a-bg w-dropdown-list"><a href="#" className="faq-a w-dropdown-link">Stories are an effective way to convey a message and help people engage with it. A story creates an emotional response in people, making the message become far more powerful, and easier to remember compared to simply showing or telling facts and figures. Studies have shown that people are far more likely to engage and recall a story and memorising via stories is a technique that has helped many of the world’s top memory athletes.</a></div>
+          </div>
+          <div className="divider"></div>
+          <div data-delay="300" data-w-id="5b89c3e3-2aff-bc3d-381f-8c1e38cd3490"  className="faq w-dropdown">
+            <div className="faq-toggle w-dropdown-toggle">
+              <div className="h5">Who can use data storytelling?</div>
+              <div  className="arrow-down qa"></div>
+            </div>
+            <div  className="faq-a-bg w-dropdown-list"><a href="#" className="faq-a w-dropdown-link">Any business or individual can use data storytelling to gain insights from evidence based reports. They are an effective way to scale information requests as well across your entire organisation. Nugit’s Data Storytelling Platform is designed for any business, from eEnterprises, through to publishers and start-ups.</a></div>
+          </div>
+          <div className="divider"></div>
+        </div>
+      </section>
+      {/* TODO Joy of data */}
     </div>
-  );
+  )
 }
 
-export const pageQuery = graphql`
-  query Telling($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
+export default ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return(
+    <DataStoryTellingPageTemplate
+      title={frontmatter.title}
+    />
+  )
+}
+
+export const DataStoryTellingPageQuery = graphql`
+  query DataStoryTelling($id: String!) {
+    markdownRemark(id: { eq: $id }) {
       frontmatter {
-        path
         title
       }
     }
