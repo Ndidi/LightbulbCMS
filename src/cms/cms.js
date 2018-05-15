ebcdf33dc0ebad18af1f951b6e7b0b29ea43b8c1
Ndidi <ndidi.alaneme@gmail.com>
2018-05-15 10:52:26 +0800

    Added config for product page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,8 @@ import CMS from 'netlify-cms'
 import "netlify-cms/dist/cms.css";
 
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('product', ProductPagePreview)
