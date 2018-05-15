76d66cd90b123ee64fa0dc354808191793b8437a
Ndidi <ndidi.alaneme@gmail.com>
2018-05-15 14:37:36 +0800

    Updated Product Preview

--- src/cms/preview-templates/ProductPagePreview.js
+++ src/cms/preview-templates/ProductPagePreview.js
@@ -7,12 +7,35 @@ const ProductPagePreview = ({entry}) => {
     <ProductPageTemplate
       title={entry.getIn(['data', 'title'])}
       subtitle={entry.getIn(['data', 'subtitle'])}
-      imageText={entry.getIn(['data', 'imageText'])}
-      textImage={entry.getIn(['data', 'textImage'])}
-      centeredContent={entry.getIn(['data', 'centeredContent'])}
-      secondTextImage={entry.getIn(['data', 'secondTextImage'])}
-      theming={entry.getIn(['data', 'theming'])}
-      connectingData={entry.getIn(['data', 'connectingData'])}
+      imageText={{
+        title: entry.getIn(['data', 'imageText', 'title']),
+        subtitle: entry.getIn(['data', 'imageText', 'subtitle']),
+        image: entry.getIn(['data', 'imageText', 'image']),
+      }}
+      textImage={{
+        title: entry.getIn(['data', 'textImage', 'title']),
+        subtitle: entry.getIn(['data', 'textImage', 'subtitle']),
+        image: entry.getIn(['data', 'textImage', 'image']),
+      }}
+      centeredContent={{
+        title: entry.getIn(['data', 'centeredContent', 'title']),
+        subtitle: entry.getIn(['data', 'centeredContent', 'subtitle']),
+        image: entry.getIn(['data', 'centeredContent', 'image']),
+      }}
+      secondTextImage={{
+        title: entry.getIn(['data', 'secondTextImage', 'title']),
+        subtitle: entry.getIn(['data', 'secondTextImage', 'subtitle']),
+        image: entry.getIn(['data', 'secondTextImage', 'image']),
+      }}
+      theming={{
+        title: entry.getIn(['data', 'theming', 'title']),
+        subtitle: entry.getIn(['data', 'theming', 'subtitle']),
+        image: entry.getIn(['data', 'theming', 'image']),
+      }}
+      connectingData={{
+        title: entry.getIn(['data', 'connectingData', 'title']),
+        subtitle: entry.getIn(['data', 'connectingData', 'subtitle']),
+      }}
     />
   )
 }
